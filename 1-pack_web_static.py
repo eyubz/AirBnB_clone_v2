0963bc1b5552211179289735c4698e5ba73aad0d
Bz14 <eyerusalembezu15@gmail.com>
2024-07-20 13:34:08 +0000

    File updated.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,16 +3,16 @@
 import os.path
 from datetime import datetime
 from fabric import task
+from invoke import Context
 
 
 @task
 def do_pack(ctx):
     dt = datetime.utcnow()
-    file = ("versions/web_static_{}{}{}{}{}{}.tgz".format(
-        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second))
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
     if not os.path.isdir("versions"):
-        if ctx.local("mkdir -p versions").failed:
+        if ctx.run("mkdir -p versions", pty=True).failed:
             return None
-    if ctx.local("tar -cvzf {} web_static".format(file)).failed:
+    if ctx.run("tar -cvzf {} web_static".format(file), pty=True).failed:
         return None
     return file
