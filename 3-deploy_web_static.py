11cea7bd2812d2fb6900f9fc0e88c25323daafcb
Bz14 <eyerusalembezu15@gmail.com>
2024-07-22 08:20:34 +0000

    File updated.

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -29,21 +29,29 @@ def do_deploy(archive_path):
     name = file.split(".")[0]
     if put(archive_path, "/tmp/{}".format(file)).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed:
+    if run("rm -rf /data/web_static/releases/{}/"
+           .format(name)).failed:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed:
+    if run("mkdir -p /data/web_static/releases/{}/"
+           .format(name)).failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)).failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+           .format(file, name)).failed:
         return False
     if run("rm /tmp/{}".format(file)).failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name)).failed:
+    st = "mv /data/web_static/releases/{}/web_static/* "
+    st += "/data/web_static/releases/{}/"
+    if run(st.format(name, name)).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed:
+    if run("rm -rf /data/web_static/releases/{}/web_static"
+           .format(name)).failed:
         return False
     if run("rm -rf /data/web_static/current").failed:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed:
+    st = "ln -s /data/web_static/releases/{}/ "
+    st += "/data/web_static/current"
+    if run(st.format(name)).failed:
         return False
     return True
 
